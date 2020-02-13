2c584ac09ae3fd21549051269057a49d9e25e488
rw4523 <58603395+rw4523@users.noreply.github.com>
2020-02-12 23:50:06 -0600

    Update MQ_Feedback.py

--- MQ_Scripts/MQ_Feedback/MQ_Feedback.py
+++ MQ_Scripts/MQ_Feedback/MQ_Feedback.py
@@ -10,15 +10,15 @@ import pandas as pd
 from pathlib import Path
 import sys
 
-# specifying which xlsx file to use ; should probably tkinter this later..
-directory = 'C:\\Path\\to\\MQs\\'              # Directory path to module quiz file directory. add the \\ at end.
+# specifying which xlsx file to use
+directory = 'C:\\Path\\to\\MQs\\'              # Directory path to module quiz file directory. Important to add the \\ at end.
 file_name = input('Enter name of Quiz .xlsx file (example: Module 1 Quiz.xlsx): ')
 
 # if the Excel extension is not already written, add it into the file name. 
 if not '.xlsx' in file_name:
     file_name = file_name + '.xlsx'
 
-# Will re-prompt until a valid file is entered
+# Will re-prompt until a valid file is entered, or exit if EXIT is typed.
 while not Path(directory + file_name).is_file():
     print('\n' + file_name + ' does not exist')
     file_name = input('Enter name of Quiz .xlsx file (example: Module 1 Quiz.xlsx) or EXIT to quit: ')
@@ -27,14 +27,14 @@ while not Path(directory + file_name).is_file():
     elif not '.xlsx' in file_name:
         file_name = file_name + '.xlsx'
     
-# prompt for what row "Quiz Questions" is on. (i.e.) if Quiz Questions is on row 12, enter 12
+# prompt for what row "Quiz Questions" is on. (i.e.) if Quiz Questions is on row 20, enter 20
 rowNum = input('What row is \'Quiz Questions\' located on: ')
 while not rowNum.isdigit():
     print('\nIntegers only')
     rowNum = input('What row is \'Quiz Questions\' located on: ')
 
 # =========================================================================================
-# ==  If using Jupyter Notebook/iPynb or similar, put below code in separate code block  ==
+# ==  If using Jupyter Notebook/ipynb or similar, put below code in separate code block  ==
 # =========================================================================================
 
 # leave this block alone.
@@ -72,15 +72,15 @@ for i in range(len(missed)):
     quiz_lo = str(found['LO'].tolist()).replace('\'', '').replace('[', '').replace(']', '') # ['A1'] --> A1
     ans_loc = str(found['Answer Location'].tolist()).replace('\'', '').replace('[', '').replace(']', '')
     
-    '''
-    found       : search value in df_qq
-    quiz_lo     : lists found(df_qq) for LO column as String
-    ans_loc     : lists found(df_qq) for Answer Location column as String
-                    (location of each answer section)
-    learn_obj   : search Symbol column for quiz_lo
-    c_obj       : lists learn_obj(df_obj) for Content column as string
-                    (content title)
-    '''
+
+#   found       : search value in df_qq
+#   quiz_lo     : lists found(df_qq) for LO column as String
+#   ans_loc     : lists found(df_qq) for Answer Location column as String
+#                   (location of each answer section)
+#   learn_obj   : search Symbol column for quiz_lo
+#   c_obj       : lists learn_obj(df_obj) for Content column as string
+#                   (content title)
+
 
     # in case of multiple answer locations, print them all
     ans_loc = ans_loc.split(' // ')
@@ -88,7 +88,7 @@ for i in range(len(missed)):
     for x in ans_loc:
         ans += ' -- (' + str(x) + ')  \n'
     
-    # these two variables used for creating the formatted output
+    # these two variables used for creating the formatted output, leave them alone
     learn_obj = ''
     c_obj = ''
     
